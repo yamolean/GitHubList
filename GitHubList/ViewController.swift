504ce53a0fd6bcd071d725f1c942a04ce0598920
yamolean <moririnmanro9608@gmail.com>
2020-02-29 22:06:04 +0900

    first commit

---
+++ GitHubList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubList
+//
+//  Created by 矢守叡 on 2020/02/29.
+//  Copyright © 2020 yamolean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

7fb1e685ab119335c68d7245eed25999639ae25d
yamolean <moririnmanro9608@gmail.com>
2020-03-01 17:00:28 +0900

    import ApiKit

--- GitHubList/ViewModel/List/ListViewModel.swift
+++ GitHubList/ViewModel/List/ListViewModel.swift
@@ -9,43 +9,44 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import Action
 
 //input:(アクション)初回トリガー,ページング
 //output:(データ)セルのデータ,navigationBarタイトル (状態)ローディング,エラー
 
-//protocol ListViewModelInput {
-//    var fetchTrigger: PublishSubject<Void> { get }
-//    var reachButtomAction: PublishSubject<Void> { get }
-//}
-//
-//protocol ListViewModelOutput {
-//    var githubRepositories: Observable<[GitHubEntity]> { get }
-//    var navigationBarTitle: Observable<String> { get }
-//    var isloading: Observable<Bool> { get }
-//    var error: Observable<NSError> { get }
-//}
-//
-//protocol ListViewModelType {
-//    var inputs: ListViewModelInput { get }
-//    var outputs: ListViewModelOutput { get }
-//}
-//
-//final class ListViewModel: ListViewModelType, ListViewModelInput, ListViewModelOutput {
-//    private let disposeBag = DisposeBag()
-//    private let searchAction:
-//
-//    var inputs: ListViewModelInput { return self }
-//    var outputs: ListViewModelOutput { return self }
-//    //input
-//    var fetchTrigger: PublishSubject<Void>
-//    var reachButtomAction: PublishSubject<Void>
-//    //output
-//    var githubRepositories: Observable<[GitHubEntity]>
-//    var navigationBarTitle: Observable<String>
-//    var isloading: Observable<Bool>
-//    var error: Observable<NSError>
-//
-//
-//
-//
-//}
+protocol ListViewModelInput {
+    var fetchTrigger: PublishSubject<Void> { get }
+    var reachButtomAction: PublishSubject<Void> { get }
+}
+
+protocol ListViewModelOutput {
+    var githubRepositories: Observable<[GitHubEntity]> { get }
+    var navigationBarTitle: Observable<String> { get }
+    var isloading: Observable<Bool> { get }
+    var error: Observable<NSError> { get }
+}
+
+protocol ListViewModelType {
+    var inputs: ListViewModelInput { get }
+    var outputs: ListViewModelOutput { get }
+}
+
+final class ListViewModel: ListViewModelType, ListViewModelInput, ListViewModelOutput {
+    private let disposeBag = DisposeBag()
+    private let searchAction: Action<Int, [GitHubEntity]>
+
+    var inputs: ListViewModelInput { return self }
+    var outputs: ListViewModelOutput { return self }
+    //input
+    var fetchTrigger: PublishSubject<Void>
+    var reachButtomAction: PublishSubject<Void>
+    //output
+    var githubRepositories: Observable<[GitHubEntity]>
+    var navigationBarTitle: Observable<String>
+    var isloading: Observable<Bool>
+    var error: Observable<NSError>
+
+    init() {
+        
+    }
+}
